eb7b875b23426772331aaf929bd9deafde014540
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2010-12-31 11:17:40 +0530

    [FIX]The published on field makes more sense as a date rather than datetime

--- cms.py
+++ cms.py
@@ -256,7 +256,7 @@ class Article(ModelSQL, ModelView, ModelPagination):
     image = fields.Many2One('nereid.static.file', 'Image')
     author = fields.Many2One('company.employee', 'Author')
     create_date = fields.DateTime('Created Date')
-    published_on = fields.DateTime('Published On')
+    published_on = fields.Date('Published On')
     sequence = fields.Integer('Sequence', required=True)
 
     def default_active(self):
