686abceee14ef4a1b8b9253b44c7e57840df0fa9
Shalabh Aggarwal <shalabh.aggarwal@openlabs.co.in>
2011-05-10 19:12:19 +0530

    [MIG]Migration from tryton 1.8 to 2.0

--- __tryton__.py
+++ __tryton__.py
@@ -2,8 +2,9 @@
 #this repository contains the full copyright notices and license terms.
 {
     'name': 'Nereid CMS',
-    'version': '1.8.0.1',
-    'author': 'Open Labs Business Solutions',
+    'version': '2.0.0.1',
+    'author': '''Open Labs Business Solutions, 
+        Openlabs Technologies & Consulting (P) Ltd.''',
     'email': 'info@openlabs.co.in',
     'website': 'http://www.openlabs.co.in/',
     'description': '''Nereid CMS''',
