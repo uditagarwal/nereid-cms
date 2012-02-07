d454a8f971bb8e94b806c427aa36a0393bfa9d57
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2012-02-07 16:29:14 -0500

    Bump version to 0.2

--- __tryton__.py
+++ __tryton__.py
@@ -2,7 +2,7 @@
 #this repository contains the full copyright notices and license terms.
 {
     'name': 'Nereid CMS',
-    'version': '2.0.0.1',
+    'version': '2.0.0.2',
     'author': '''Open Labs Business Solutions, 
         Openlabs Technologies & Consulting (P) Ltd.''',
     'email': 'info@openlabs.co.in',
